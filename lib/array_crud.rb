0d08b5cbd44c070bcf2691210aa5c2ceaafe5da9
Nerly Ton <nerlyton@gmail.com>
2020-07-29 17:05:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
-def create_an_empty_array
-  
+def create_an_empty_array 
+  []
 end
 
 def create_an_array
-  
+  cats = ["menina", "galinda", "hinata", "shikamaru"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push ("element")
+  puts array
 end
 
 def add_element_to_start_of_array(array, element)
